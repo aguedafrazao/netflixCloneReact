2960ac00e346f6f70239134d44374a5e51b3d1e2
aguedafrazao <aguedaaraujo13@gmail.com>
2021-07-07 08:44:48 -0300

    show lists on page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Tmdb from './Tmdb';
 
 export default () => {
+  // mostar a lista na tela
+  const [movieList, setMovieList] = useState([]);
+  // qnd a tela for carregada ele vai usar a funcao q eu digitar aqui 
+  useEffect(()=>{
+    const loadAll = async () => {
+      // pegando a lista completa
+      let list = await Tmdb.getHomeList();
+      setMovieList(list);
+    }
+
+    loadAll();
+  }, []);
   return (
-    <div>
-      Olá
+    <div className="page">
+      <section className="lists">
+        {movieList.map((item, key) => (
+          <div>
+            {item.title}
+          </div>
+        ))}
+      </section>
     </div>
   )
 }
--- src/Tmdb.js
+++ src/Tmdb.js
@@ -24,7 +24,7 @@ export default {
             {
                 slug: 'toprated',
                 title: 'Em alta',
-                items: await basicFetch(`/movie/top_reted?language=pt-BR&api_key=${API_KEY}`)
+                items: await basicFetch(`/movie/top_rated?language=pt-BR&api_key=${API_KEY}`)
             },
             {
                 slug: 'actions',
