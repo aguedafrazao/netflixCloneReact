eebf79eeded68bb48a8207250c16c83cd5d8ed8b
aguedafrazao <aguedaaraujo13@gmail.com>
2021-07-07 10:17:30 -0300

    creating menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
 import React, { useEffect, useState } from 'react';
+import './App.css'
 import Tmdb from './Tmdb';
 import MovieRow from './components/MovieRow';
-
-
+import FeaturedMovie from './components/FeaturedMovie';
 
 export default () => {
   // mostar a lista na tela
   const [movieList, setMovieList] = useState([]);
+  const [featuredData, setFeaturedData] = useState(null);
+
   // qnd a tela for carregada ele vai usar a funcao q eu digitar aqui 
   useEffect(() => {
     const loadAll = async () => {
       // pegando a lista completa
       let list = await Tmdb.getHomeList();
       setMovieList(list);
+      // Pegar o featured
+      let originals = list.filter(i => i.slug === 'originals');
+      let randomChosen = Math.floor(Math.random() * (originals[0].items.results.length - 1));
+      let chosen = originals[0].items.results[randomChosen];
+      let chosenInfo = await Tmdb.getMovieInfo(chosen.id, 'tv');
+      setFeaturedData(chosenInfo);
     }
 
     loadAll();
   }, []);
   return (
     <div className="page">
+      {featuredData &&
+        <FeaturedMovie item={featuredData} />
+      }
       <section className="lists">
         {movieList.map((item, key) => (
           <MovieRow key={key} title={item.title} items={item.items}/>
